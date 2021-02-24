9d706c58353023e592d3769da271f066937f029c
AJP2511 <johnson2511@gmail.com>
2021-02-24 02:19:00 -0300

    changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/Global.css";
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps}></Component>;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
+import Head from "next/head";
 import { ExperienceBar } from "../components/ExperienceBar";
 import styles from "../styles/pages/Home.module.css";
 import Profile from "../components/Profile";
 import CompletedChallenges from "../components/CompletedChallenges";
 import Countdown from "../components/Countdown";
-import Head from "next/head";
 
 export default function Home() {
   return (
